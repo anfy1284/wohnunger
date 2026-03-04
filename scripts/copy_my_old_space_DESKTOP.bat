f0d905373222d41ceb3d915072f2cf8bfb424502
Malewisch A. <anfy1284@gmail.com>
2026-03-04 17:26:04 +0100

    Update my-old-space dependency version in package-lock.json and adjust source path in copy_my_old_space_DESKTOP.bat

--- scripts/copy_my_old_space_DESKTOP.bat
+++ scripts/copy_my_old_space_DESKTOP.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set "SRC=E:\Andrey\old-space-app\node_modules\my-old-space"
+set "SRC=D:\wohnunger\node_modules\my-old-space"
 set "DEST=E:\Andrey\my-old-space"
 
 xcopy "%SRC%" "%DEST%" /E /I /Y /H /C /Q
