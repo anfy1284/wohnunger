9947ed89052f03538bfa16e04fd232f4ac8dbe84
Malewisch A. <anfy1284@gmail.com>
2026-03-15 19:09:17 +0100

    refactor: remove unused test and debug files, add debugModels.js for database model initialization

--- test_admin2.js
+++
@@ -1,4 +0,0 @@
-const { Sequelize } = require('sequelize');
-const s = require('./dbSettings.postgres.json');
-const seq = new Sequelize(s.database, s.username, s.password, { host: s.host, port: s.port, dialect: 'postgres', logging: false });
-seq.query(`SELECT u.*, us."roleId" FROM users u LEFT JOIN user_systems us ON u."UID" = us."userId" WHERE u.name = 'admin'`).then(r => { console.log(r[0]); seq.close(); });
